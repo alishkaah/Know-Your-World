b00766fe2bedceb35248676b3d9c7d2ed5e404ce
Moringa <alinoorshukri8@gmail.com>
2022-06-24 17:45:49 +0300

    added details of each country from api

--- JS/app.js
+++ JS/app.js
@@ -30,6 +30,34 @@ searchBtn.addEventListener("click", () => {
                 <h4>Capital:</h4>
                 <span>${data[0].capital[0]}</span>
             </div>
+            <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${data[0].population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency:</h4>
+                <span>${
+                  data[0].currencies[Object.keys(data[0].currencies)].name
+                } - ${Object.keys(data[0].currencies)[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common Languages:</h4>
+                <span>${Object.values(data[0].languages)
+                  .toString()
+                  .split(",")
+                  .join(", ")}</span>
+            </div>
         </div>`
     })
 })
