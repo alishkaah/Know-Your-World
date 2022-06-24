41f4eeed0bb6581407b13ac3236051f99960effe
Moringa <alinoorshukri8@gmail.com>
2022-06-24 15:55:19 +0300

    fetched data chnaged to json and conslled returned data to check if it meets my requirements

--- JS/app.js
+++ JS/app.js
@@ -14,4 +14,10 @@ searchBtn.addEventListener("click", () => {
     let countryName = countryInp.value;
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     // console.log(finalURL);
+    fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log(data);
+        console.log(data[0]);
+    })
 })
