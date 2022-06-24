c255a9dbd87f492d21416a259269d69c9a5ee984
Moringa <alinoorshukri8@gmail.com>
2022-06-24 15:47:55 +0300

    On click the button my api url invoked

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,17 @@
+
+
+//nav-bar toggle 
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-nav-toggle");
 
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle("active");
 });
+
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("country-input");
+searchBtn.addEventListener("click", () => {
+    let countryName = countryInp.value;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    // console.log(finalURL);
+})
