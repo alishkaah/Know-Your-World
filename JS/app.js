2c6b5de0229dee692c88b37d72e6d3b3e9326216
Moringa <alinoorshukri8@gmail.com>
2022-06-24 17:16:12 +0300

    insert content into a result empty div from fetched data

--- JS/app.js
+++ JS/app.js
@@ -10,6 +10,7 @@ navBarToggle.addEventListener("click", function() {
 
 let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-input");
+let result  = document.getElementById('result');
 searchBtn.addEventListener("click", () => {
     let countryName = countryInp.value;
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
@@ -18,6 +19,17 @@ searchBtn.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => {
         // console.log(data);
-        console.log(data[0]);
+        // console.log(data[0]);
+        // console.log(Object.keys(data[0].currencies)[0]);
+        // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+        result.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
+        </div>`
     })
 })
