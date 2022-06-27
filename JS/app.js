384de83ee1a0ea780892d5082b7f4e4fece6c086
Moringa <alinoorshukri8@gmail.com>
2022-06-27 23:03:46 +0300

    removed unnecesary comments

--- JS/app.js
+++ JS/app.js
@@ -28,8 +28,6 @@ async function getResult(){
     .then((response) => response.json())
     .then((data) => {
         // console.log(data);
-        // console.log(data[0]);
-        // console.log(Object.keys(data[0].currencies)[0]);
         // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
         result.innerHTML = `
         <img src="${data[0].flags.svg}" class="flag-img">
